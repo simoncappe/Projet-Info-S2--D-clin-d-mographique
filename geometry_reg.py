a8e1a7fd13061386d4503a89b11f931b449a85b6
Simon Cappe <simon.cappe@etu.minesparis.psl.eu>
2023-03-14 09:04:23 +0100

    MultiPolygon assimilés

--- geometry_reg.py
+++ geometry_reg.py
@@ -5,8 +5,32 @@ import geopandas
 from shapely.geometry import Point, LineString, Polygon, MultiPolygon
 
 
+g=pd.read_json('geometry.json')
+
+#Fichier sencé contenir les frontières de toute les régions de France, à explorer
 
 
+Names=[]
+Id=[]
+G=[]
+for i in g['features']:
+    Names.append(i['properties']['libgeo'])
+    Id.append(i['properties']['reg'])
+    
+    if i['geometry']['type']=='Polygon':
+        
+        p=i['geometry']['coordinates']
+        G.append(Polygon(p[0]))
+    else: 
+        mp=i['geometry']['coordinates']
+        s=[Polygon(p[0]) for p in mp]
+        G.append(MultiPolygon(s))
+        
+d=pd.DataFrame(Names,columns=['Nom'])
+d['Id']=Id
+
+
+#gd=geopandas.GeoDataFrame(d,geometry=G)
 
 
 
@@ -15,38 +39,15 @@ df=pd.read_csv('regiondemographique.csv',usecols=['REG_ID', 'Région',
 
 #regiondemographique.csv est une exportation du site de l'OCDE que j'ai effectuée
 
-df=df[(df.Indicateur==df['Indicateur'][0]) & (df.Année==2005)&(df.SEX=='T') ].iloc[0:8]
+df=df[(df.Indicateur==df['Indicateur'][0]) & (df.Année==2005)&(df.SEX=='T') ]
+df.reset_index()
 #Je veux le Taux de mortalité régional en 2005 pour les hommes ET les femmes (il existe des données différenciées)
-
-g=pd.read_json('geometry.json')
-#Fichier sencé contenir les frontières de toute les régions de France, à explorer
-g=g.iloc[0:12]
+print(df)
 
 
+gd=geopandas.GeoDataFrame(df,geometry=G[1:13])
 
-#Le fichier json contient des dictionnaires avec des types Polygon ou Multipolygon
-#et puis les coordonnées correspondantes
-#Je ne comprend pas trop comment fonctionne les multipolygon pour l'instant mais on 
-#arrive à faire quelques trucs en utilisant seulement les polygon, qui nous donnent 
-#des frontières acceptables
-Poly=[]
-MultiPoly=[]
-for i in g['features']: 
-    if i['geometry']['type']=='Polygon':
-        Poly+=i['geometry']['coordinates']
-    else:
-        MultiPoly+=i['geometry']['coordinates'][0]
-G=[]
-for i in Poly:
-    G.append(Polygon(i))
-
-
-gpd=geopandas.GeoDataFrame(df,geometry=G)
-gpd.plot("Value",legend=True)
-plt.title('Taux de mortalité (décès pour 1000 habitants)')
+gd.plot("Value",legend=True)
+plt.title('Taux de mortalité')
 plt.legend()
-#Une carte incomplète mais une piste en plus
 plt.show()
-    
-
-
