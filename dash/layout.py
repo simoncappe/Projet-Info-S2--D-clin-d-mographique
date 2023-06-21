587edef7be5554b845cc7e40c597eba205fe8a15
Simon Cappe <simon.cappe@etu.minesparis.psl.eu>
2023-06-21 15:46:31 +0200

    logo intégrés

--- dash/layout.py
+++ dash/layout.py
@@ -300,24 +300,24 @@ layout = html.Div(
 # fin du layout
 
 #description des variables
-des = html.Div(children=[
-    html.H1(children='Description des variables'),
+des = html.Div(id = 'description',children=[
+    html.H1(id = 'Des',children='Description des variables'),
     html.Ul(id='list',
                                         children=[
                                             html.Li(id='POPINC',
-                                                    children="Variation de la Population : Incrément de population au sein de la commune entre le premier Janvier de l\'année initiale et le premier Janvier de l\'année finale"
+                                                    children=[html.Img(id = 'popincimg',src = 'assets/popincimg.PNG'),"Variation de la Population : Incrément de population au sein de la commune entre le premier Janvier de l\'année initiale et le premier Janvier de l\'année finale"]
                                                     ),
                                             html.Li(id="NETNAT",
-                                                    children="Solde naturel : Différence entre le nombre de naissances et le nombre de décès enregistrés au sein de la commune entre le premier Janvier de l\'année initiale et le premier Janvier de l\'année finale "
+                                                    children=[html.Img(id = 'netnatimg',src = 'assets/netnatimg.PNG'),"Solde naturel : Différence entre le nombre de naissances et le nombre de décès enregistrés au sein de la commune entre le premier Janvier de l\'année initiale et le premier Janvier de l\'année finale "]
                                                     ),
                                             html.Li(id="NETMOB",
-                                                    children="Solde migratoire interne : Différence entre le nombre de personnes ayant emménagé dans la commune qui vivaient déjà en France et le nombre de personne vivant dans la commune et qui ont déménagé autre part en France entre le premier Janvier de l\'année initiale et le premier Janvier de l\'année finale"
+                                                    children=[html.Img(id = "netmobimg",src = 'assets/netmobimg.PNG'),"Solde migratoire interne : Différence entre le nombre de personnes ayant emménagé dans la commune qui vivaient déjà en France et le nombre de personne vivant dans la commune et qui ont déménagé autre part en France entre le premier Janvier de l\'année initiale et le premier Janvier de l\'année finale"]
                                                     ),
                                             html.Li(id="NETMIG",
-                                                    children="Solde migratoire externe : Différence entre le nombre de personnes ayant emménagé dans la commune qui vivaient à l'étranger et le nombre de personnes vivant dans la commune et qui ont déménagé à l'étranger"
+                                                    children=[html.Img(id = "netmigimg",src = 'assets/netmigimg.PNG'),"Solde migratoire externe : Différence entre le nombre de personnes ayant emménagé dans la commune qui vivaient à l'étranger et le nombre de personnes vivant dans la commune et qui ont déménagé à l'étranger"]
                                                     ),
                                             html.Li(id="C",
-                                                    children="Cause du changement de population : Maximum en valeur absolue des trois composantes présentées ci-dessus, accompagnée d'un indicateur montrant si la population a augmenté ou diminué"
+                                                    children=[html.Img(id = "causeimg",src = 'assets/causeimg.PNG'),"Cause du changement de population : Maximum en valeur absolue des trois composantes présentées ci-dessus, accompagnée d'un indicateur montrant si la population a augmenté ou diminué"]
                                                     )
                                         ]
                                         )
